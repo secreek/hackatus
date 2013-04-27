5b99da44e5c600aec40540083dc07499be442953
Void Main <voidmain1313113@gmail.com>
2013-04-28 00:46:03 +0800

    first working version

---
+++ github.rb
@@ -0,0 +1,23 @@
+require 'json'
+require './network_utils'
+
+class Github
+  include NetworkUtils
+
+  def self.prepare_gh_key_pair
+    params = {
+      "client_id" => ENV["GH_KEY_PAIR_ID"],
+      "client_secret" => ENV["GH_KEY_PAIR_SECRET"]
+    }
+
+    params
+  end
+
+  def self.commit_count repo_name, since
+    user_uri = "https://api.github.com/repos/#{repo_name}/commits?since=#{since}"
+    puts user_uri
+    params = self.prepare_gh_key_pair
+    return NetworkUtils.do_request_returning_json(user_uri, params).length
+  end
+
+end
--- server.rb
+++ server.rb
@@ -1,5 +1,28 @@
 require 'sinatra'
+require 'json'
+require './github'
 
-get '/' do
-  "hello, world!"
+get '/summary' do
+  summary = {
+    "title" => "Hackathon Status",
+    "refreshEveryNSeconds" => 60
+  }
+  commit_summary = []
+  obj = JSON.load(open('config.json').read)
+  since = obj["since"]
+  obj["repos"].each do |repo|
+    commit_info = {
+      "title" => repo["name"],
+      "value" => Github.commit_count(repo["path"], since)
+    }
+    commit_summary << commit_info
+  end
+
+  commit_summary.sort! do |first, second|
+    puts second
+    puts first
+    second["value"] - first["value"]
+  end
+  summary["datapoints"] = commit_summary
+  summary.to_json
 end
